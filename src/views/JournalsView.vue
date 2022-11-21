b18af5f14c02972e830a6e2a0b88844d7d221607
max <max.lillie95@gmail.com>
2022-11-20 22:58:54 -0600

    changed journal's route to be a v-for skeleton

--- src/views/JournalsView.vue
+++ src/views/JournalsView.vue
@@ -1,17 +1,22 @@
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
       message: "Welcome to my Journals!",
+      journalPosts: [],
     };
   },
-  created: function () { },
+  created: function () {
+    this.journalPostIndex();
+  },
   methods: {
     journalPostIndex: function () {
       axios.get("http://localhost:3000/blogs").then(response => {
         // console.log(response.data)
         this.journalPosts = response.data
-        this.journalPost = this.journalPosts[this.journalPosts.length - 1]
+        // this.journalPosts = this.journalPosts.reverse()
         // console.log(this.journalPosts)
         // console.log(this.journalPost)
       })
@@ -26,41 +31,48 @@ export default {
       <div class="container">
         <header class="major">
           <h2>My Work Journals</h2>
-          <p>Here is were I will post a weekly journa of my</p>
+          <p>Here is were I will have a weekly journal post of my progress into learning to code!</p>
         </header>
 
         <!-- Content -->
-        <section id="content">
-          <a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
-          <h3>Dolore Amet Consequat</h3>
-          <p>Aliquam massa urna, imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros
-            ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non,
-            malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros
-            ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam cubilia adipiscing interdum aliquet
-            aliquet placerat cubilia accumsan cep ante nullam adipiscing tortor odio. Iaculis adipiscing accumsan
-            nascetur in cras. Lacinia placerat.</p>
-          <p>Vestibulum diam quam, mollis at consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum.
-            Morbi mattis elementum orci, nec dictum porta cursus justo. Quisque ultricies lorem in ligula condimentum,
-            et egestas turpis sagittis. Cras ac nunc urna. Nullam eget lobortis purus. Phasellus vitae tortor non est
-            placerat tristique faucibus magna adipiscing aliquet blandit nisi dolor accumsan nascetur lorem. Vis enim.
-            Curae. Mi arcu vis aliquam ipsum non accumsan ac mi faucibus amet eu accumsan erat interdum semper accumsan.
-            Eu sagittis tellus accumsan neque lorem penatibus in porttitor. Vivamus commodo curae. Condimentum lobortis
-            praesent id ac lorem. Lorem vis ipsum dolor feugiat consequat dolore adipiscing.</p>
-          <h3>Sed Magna Ornare</h3>
-          <p>In vestibulum massa quis arcu lobortis tempus. Nam pretium arcu in odio vulputate luctus. Suspendisse
-            euismod lorem eget lacinia fringilla. Sed sed felis justo. Nunc sodales elit in laoreet aliquam. Nam
-            gravida, nisl sit amet iaculis porttitor, risus nisi rutrum metus. In accumsan lorem nunc ultrices a ac eu
-            accumsan ac id nullam. Nec lobortis. Faucibus. Mi commodo laoreet arcu at non mi lacinia aliquet non
-            lobortis phasellus accumsan nascetur varius. Integer orci porttitor eu faucibus aliquet faucibus ac accumsan
-            adipiscing accumsan odio. Ut adipiscing.</p>
-          <ul>
-            <li>Faucibus orci lobortis ac adipiscing integer.</li>
-            <li>Col accumsan arcu mi aliquet placerat.</li>
-            <li>Lobortis vestibulum ut magna tempor massa nascetur.</li>
-            <li>Blandit massa non blandit tempor interdum.</li>
-            <li>Lacinia mattis arcu nascetur lobortis.</li>
-          </ul>
-        </section>
+        <div v-for="journalPost in this.journalPosts">
+          <section id="content">
+            <a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+            <h3>Dolore Amet Consequat</h3>
+            <p>Aliquam massa urna, imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel
+              eros
+              ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur
+              non,
+              malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros
+              ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam cubilia adipiscing interdum aliquet
+              aliquet placerat cubilia accumsan cep ante nullam adipiscing tortor odio. Iaculis adipiscing accumsan
+              nascetur in cras. Lacinia placerat.</p>
+            <p>Vestibulum diam quam, mollis at consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum.
+              Morbi mattis elementum orci, nec dictum porta cursus justo. Quisque ultricies lorem in ligula condimentum,
+              et egestas turpis sagittis. Cras ac nunc urna. Nullam eget lobortis purus. Phasellus vitae tortor non est
+              placerat tristique faucibus magna adipiscing aliquet blandit nisi dolor accumsan nascetur lorem. Vis enim.
+              Curae. Mi arcu vis aliquam ipsum non accumsan ac mi faucibus amet eu accumsan erat interdum semper
+              accumsan.
+              Eu sagittis tellus accumsan neque lorem penatibus in porttitor. Vivamus commodo curae. Condimentum
+              lobortis
+              praesent id ac lorem. Lorem vis ipsum dolor feugiat consequat dolore adipiscing.</p>
+            <h3>Sed Magna Ornare</h3>
+            <p>In vestibulum massa quis arcu lobortis tempus. Nam pretium arcu in odio vulputate luctus. Suspendisse
+              euismod lorem eget lacinia fringilla. Sed sed felis justo. Nunc sodales elit in laoreet aliquam. Nam
+              gravida, nisl sit amet iaculis porttitor, risus nisi rutrum metus. In accumsan lorem nunc ultrices a ac eu
+              accumsan ac id nullam. Nec lobortis. Faucibus. Mi commodo laoreet arcu at non mi lacinia aliquet non
+              lobortis phasellus accumsan nascetur varius. Integer orci porttitor eu faucibus aliquet faucibus ac
+              accumsan
+              adipiscing accumsan odio. Ut adipiscing.</p>
+            <ul>
+              <li>Faucibus orci lobortis ac adipiscing integer.</li>
+              <li>Col accumsan arcu mi aliquet placerat.</li>
+              <li>Lobortis vestibulum ut magna tempor massa nascetur.</li>
+              <li>Blandit massa non blandit tempor interdum.</li>
+              <li>Lacinia mattis arcu nascetur lobortis.</li>
+            </ul>
+          </section>
+        </div>
 
       </div>
     </div>
