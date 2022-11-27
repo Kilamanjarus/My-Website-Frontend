ff02a6113d2e663c4a469d7f949466484928638d
max <max.lillie95@gmail.com>
2022-11-27 15:37:19 -0600

    art page is finished, half of interests page is done

--- src/views/MyArtView.vue
+++ src/views/MyArtView.vue
@@ -11,8 +11,140 @@ export default {
 </script>
 
 <template>
-  <div class="home">
-    <h1>{{ message }}</h1>
+  <div id="main" class="wrapper style1">
+    <div class="container">
+      <header class="major">
+        <h2>My Sketchfab</h2>
+        <p>Here is where I post of a lot of my 3D art that isn't under NDA!</p>
+      </header>
+      <!-- Hydralisk -->
+      <div class="row gtr-150">
+        <div class="col-8 col-12-medium">
+
+          <!-- Content -->
+          <section id="content">
+            <div class="sketchfab-embed-wrapper"> <iframe title="Hydralisk Sculpt" frameborder="0" allowfullscreen
+                mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"
+                xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="720"
+                height="480" src="https://sketchfab.com/models/0c201f805e0749908cd4021c82a56cfb/embed"> </iframe>
+            </div>
+          </section>
+        </div>
+        <div class="col-4 col-12-medium">
+          <!-- Sidebar -->
+          <section id="sidebar">
+            <section>
+              <h3>Hydralisk Sculpt</h3>
+              <div>This was a sculpt done in Zbrush, my main focus on this model was to focus on the the weird muscular
+                flesh that the Zerg from Starcraft have! </div>
+              <div>
+                Looking back on it now, I would love to go back and flesh out the joints between the limbs a lot better,
+                I've learned new ways of creating the fleshy bulbs also!</div>
+              <br />
+              <footer>
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a
+                    href="https://sketchfab.com/3d-models/hydralisk-sculpt-0c201f805e0749908cd4021c82a56cfb?utm_medium=embed&utm_campaign=share-popup&utm_content=0c201f805e0749908cd4021c82a56cfb"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;"> Hydralisk Sculpt </a> by <a
+                    href="https://sketchfab.com/Kilamanjarus?utm_medium=embed&utm_campaign=share-popup&utm_content=0c201f805e0749908cd4021c82a56cfb"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;"> Kilamanjarus </a> on <a
+                    href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=0c201f805e0749908cd4021c82a56cfb"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p>
+              </footer>
+            </section>
+            <hr />
+          </section>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Samurai -->
+    <div class="container">
+      <header class="major">
+      </header>
+      <div class="row gtr-150">
+        <div class="col-4 col-12-medium">
+          <!-- Sidebar -->
+          <section id="sidebar">
+            <section>
+              <h3>VRChat Samurai</h3>
+              <p>This is my personal VRChat model, I had orignally wanted to have to jet black coloring be a
+                smoke-like effect, but I need to do more research into unity to see if it is possible.
+                <br />
+                A few things I aimed to do in this first iteration of my Samurai was to get the poly count to as
+                close as 20k as possible, due to the limitations of some VR headsets not allowing for models over
+                that limit to be processed. Two, was to completely color it, my biggest weakness in my art, myself.
+                Both of which I am happy to say I acheived a fairly decent approximation of what I want for a first
+                go!
+              </p>
+              <footer>
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a
+                    href="https://sketchfab.com/3d-models/vrchat-samurai-f538436a104a472887a653d1d6650665?utm_medium=embed&utm_campaign=share-popup&utm_content=f538436a104a472887a653d1d6650665"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;"> Vrchat Samurai </a> by <a
+                    href="https://sketchfab.com/Kilamanjarus?utm_medium=embed&utm_campaign=share-popup&utm_content=f538436a104a472887a653d1d6650665"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;"> Kilamanjarus </a> on <a
+                    href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=f538436a104a472887a653d1d6650665"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p>
+              </footer>
+            </section>
+          </section>
+          <hr />
+        </div>
+        <div class="col-8 col-12-medium">
+
+          <!-- Content -->
+          <section id="content">
+            <div class="sketchfab-embed-wrapper"> <iframe title="Vrchat Samurai" frameborder="0" allowfullscreen
+                mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"
+                xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="720"
+                height="480" src="https://sketchfab.com/models/f538436a104a472887a653d1d6650665/embed"> </iframe>
+
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <header class="major">
+      </header>
+      <!-- Cupcake -->
+      <div class="row gtr-150">
+        <div class="col-8 col-12-medium">
+
+          <!-- Content -->
+          <section id="content">
+            <div class="sketchfab-embed-wrapper"> <iframe title="Cupcake Finished" frameborder="0" allowfullscreen
+                mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"
+                xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="720"
+                height="480" src="https://sketchfab.com/models/7b4a994e9ff8481895166dc4e2f624dd/embed"> </iframe>
+            </div>
+          </section>
+        </div>
+        <div class="col-4 col-12-medium">
+          <!-- Sidebar -->
+          <section id="sidebar">
+            <section>
+              <h3>Monster Cupcake</h3>
+              <div>This was the main character/creature for my college final project, model and rigged by myself, and
+                animated and textured by my classmate, Casey Olson</div>
+              <div>
+                Just looking back on this project makes me want to go back and remake it completely as a movie! I miss
+                this class, was a great time!</div>
+              <br />
+              <footer>
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a
+                    href="https://sketchfab.com/3d-models/cupcake-finished-7b4a994e9ff8481895166dc4e2f624dd?utm_medium=embed&utm_campaign=share-popup&utm_content=7b4a994e9ff8481895166dc4e2f624dd"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;"> Cupcake Finished </a> by <a
+                    href="https://sketchfab.com/Kilamanjarus?utm_medium=embed&utm_campaign=share-popup&utm_content=7b4a994e9ff8481895166dc4e2f624dd"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;"> Kilamanjarus </a> on <a
+                    href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=7b4a994e9ff8481895166dc4e2f624dd"
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p>
+              </footer>
+            </section>
+          </section>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/views/RandomInterestView.vue
+++ src/views/RandomInterestView.vue
@@ -2,7 +2,7 @@
 export default {
   data: function () {
     return {
-      message: "Welcome to my Interests!",
+      message: "My two current fixations!",
     };
   },
   created: function () { },
@@ -12,7 +12,106 @@ export default {
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <!-- Permaculture -->
+    <div id="main" class="wrapper style1">
+      <div class="container">
+        <header class="major">
+          <h2>Permaculture</h2>
+          <p>"The development of agricultural ecosystems intended to be sustainable and self-sufficient" - Oxford
+            Languages</p>
+        </header>
+
+        <!-- Content -->
+        <section id="content">
+          <a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+          <h3>Why Care?</h3>
+          <p>"Microplastics are ubiquitous across ecosystems, yet the exposure risk to humans is unresolved. Focusing on
+            the American diet, we evaluated the number of microplastic particles in commonly consumed foods in relation
+            to their recommended daily intake. The potential for microplastic inhalation and how the source of drinking
+            water may affect microplastic consumption were also explored. Our analysis used 402 data points from 26
+            studies, which represents over 3600 processed samples. Evaluating approximately 15% of Americans’ caloric
+            intake, we estimate that annual microplastics consumption ranges from 39000 to 52000 particles depending on
+            age and sex. These estimates increase to 74000 and 121000 when inhalation is considered. Additionally,
+            individuals who meet their recommended water intake through only bottled sources may be ingesting an
+            additional 90000 microplastics annually, compared to 4000 microplastics for those who consume only tap
+            water. These estimates are subject to large amounts of variation; however, given methodological and data
+            limitations, these values are likely underestimates."<br /> <a
+              href="https://pubs.acs.org/doi/full/10.1021/acs.est.9b01517">Source</a> </p>
+          <p>Just reading those words makes me want to swap everything I eat and drink to organicly produced. The
+            American Food system has been contaminated by coorporate greed, and I plan to create a homestead for my
+            family where I can raise my own livestock and grown my own food. To do this, I've researched into a few
+            different ways of growing food, and a lot of what I came back with was wasteful on something, be that water,
+            area, or even ground quality in the long term. Permaculter seems to be the perfect answer to every problem
+            that we face in our current style of farming.</p>
+          <h3>Why Does Permaculture Work?</h3>
+          <p>"The philosophy behind permaculture is one of working with, rather than against, nature; of protracted and
+            thoughtful observation rather than protracted and thoughtless action; of looking at systems in all their
+            functions, rather than asking only one yield of them; and allowing systems to demonstrate their own
+            evolutions."</p>
+          <ul>
+            <li>"The conscious design and maintenance of agriculturally productive ecosystems which have the diversity,
+              stability, and resilience of natural ecosystems.""</li>
+            <li>"It is the harmonious integration of landscape and people — providing their food, energy, shelter, and
+              other material and non-material needs in a sustainable way.""</li>
+            <li>"It is a multidisciplinary toolbox including agriculture, water harvesting and hydrology, energy,
+              natural
+              building, forestry, waste management, animal systems, aquaculture, appropriate technology, economics and
+              community development."</li>
+          </ul>
+          <a href="https://www.permaculturenews.org/what-is-permaculture/">Source</a>
+        </section>
+
+      </div>
+    </div>
+
+
+
+
+
+    <!-- Aquaponics -->
+    <div id="main" class="wrapper style1">
+      <div class="container">
+        <header class="major">
+          <h2>Aquaponics</h2>
+          <p>"A system of aquaculture in which the waste produced by farmed fish or other aquatic animals supplies
+            nutrients for plants grown hydroponically, which in turn purify the water." - Oxford Languages</p>
+        </header>
+
+        <!-- Content -->
+        <section id="content">
+          <a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+          <h3>Dolore Amet Consequat</h3>
+          <p>Aliquam massa urna, imperdiet sit amet mi non, bibendum euismod est. Curabitur mi justo, tincidunt vel eros
+            ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam quam, mollis at magna consectetur non,
+            malesuada quis augue. Morbi tincidunt pretium interdum est. Curabitur mi justo, tincidunt vel eros
+            ullamcorper, porta cursus justo. Cras vel neque eros. Vestibulum diam cubilia adipiscing interdum aliquet
+            aliquet placerat cubilia accumsan cep ante nullam adipiscing tortor odio. Iaculis adipiscing accumsan
+            nascetur in cras. Lacinia placerat.</p>
+          <p>Vestibulum diam quam, mollis at consectetur non, malesuada quis augue. Morbi tincidunt pretium interdum.
+            Morbi mattis elementum orci, nec dictum porta cursus justo. Quisque ultricies lorem in ligula condimentum,
+            et egestas turpis sagittis. Cras ac nunc urna. Nullam eget lobortis purus. Phasellus vitae tortor non est
+            placerat tristique faucibus magna adipiscing aliquet blandit nisi dolor accumsan nascetur lorem. Vis enim.
+            Curae. Mi arcu vis aliquam ipsum non accumsan ac mi faucibus amet eu accumsan erat interdum semper accumsan.
+            Eu sagittis tellus accumsan neque lorem penatibus in porttitor. Vivamus commodo curae. Condimentum lobortis
+            praesent id ac lorem. Lorem vis ipsum dolor feugiat consequat dolore adipiscing.</p>
+          <h3>Sed Magna Ornare</h3>
+          <p>In vestibulum massa quis arcu lobortis tempus. Nam pretium arcu in odio vulputate luctus. Suspendisse
+            euismod lorem eget lacinia fringilla. Sed sed felis justo. Nunc sodales elit in laoreet aliquam. Nam
+            gravida, nisl sit amet iaculis porttitor, risus nisi rutrum metus. In accumsan lorem nunc ultrices a ac eu
+            accumsan ac id nullam. Nec lobortis. Faucibus. Mi commodo laoreet arcu at non mi lacinia aliquet non
+            lobortis phasellus accumsan nascetur varius. Integer orci porttitor eu faucibus aliquet faucibus ac accumsan
+            adipiscing accumsan odio. Ut adipiscing.</p>
+          <ul>
+            <li>Faucibus orci lobortis ac adipiscing integer.</li>
+            <li>Col accumsan arcu mi aliquet placerat.</li>
+            <li>Lobortis vestibulum ut magna tempor massa nascetur.</li>
+            <li>Blandit massa non blandit tempor interdum.</li>
+            <li>Lacinia mattis arcu nascetur lobortis.</li>
+          </ul>
+        </section>
+
+      </div>
+    </div>
   </div>
 </template>
 
