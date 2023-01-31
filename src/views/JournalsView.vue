653c5ccc4a469699de4489e8d102c245a5010170
max <max.lillie95@gmail.com>
2023-01-31 14:02:00 -0600

    reversing the sort and it will be correct

--- src/views/JournalsView.vue
+++ src/views/JournalsView.vue
@@ -16,7 +16,7 @@ export default {
       axios.get("https://kilamanjarus-me.herokuapp.com/blogs.json").then(response => {
         // console.log(response.data)
         this.journalPosts = response.data
-        this.journalPosts.sort((a, b) => a.id - b.id)
+        this.journalPosts.sort((a, b) => b.id - a.id)
         // this.journalPosts = this.journalPosts.reverse()
         // console.log(this.journalPosts)
         // console.log(this.journalPost)
